4b7fbe77a0f2731893db74175006719b97544b92
Bruno <luna.brsilva@gmail.com>
2021-05-03 17:29:36 -0300

    exercicios de if/else

---
+++ ex008.js
@@ -0,0 +1,8 @@
+var vel = 30.5
+console.log (`A velocidade do seu carro é de ${vel} KM/H.`)
+
+if (vel > 60){  //condição simples
+    console.log (`Sua velocidade foi acima do permitido. Multado`)
+}
+
+console.log (`Lembre-se: Dirija sempre usando o cinto de segurança`)
