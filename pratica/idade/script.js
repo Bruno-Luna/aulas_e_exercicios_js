6d697cc132953c882f5b771007bd7f9a1fd8d3bf
Bruno <luna.brsilva@gmail.com>
2021-05-07 17:49:40 -0300

    final-verificador-de-idade

--- pratica/idade/script.js
+++ pratica/idade/script.js
@@ -14,41 +14,47 @@ function verificar(){
         var genero = ''
         if (fsex[0].checked){
             genero = 'homem'
-            if (idade >= 0 && idade < 11){
+            if (idade >= 0 && idade <= 11){
             // criança
             img.setAttribute('src', 'foto-8.png')
             img.style.display = "block"
 
-            }else if (idade < 21){
+            }else if (idade >= 12 && idade <= 21){
                 // adolescente
-                img.setAttribute('src', 'https://cdn.12min.com/books/books_background/60606_o-cerebro-adolescente.site_cover.jpg?1576600447')
-            }else if (idade < 50 ){
+                img.setAttribute('src', 'foto-4.png')
+                img.style.display = "block"
+            }else if (idade >= 22 && idade <= 59 ){
                 // adulto
-                img.setAttribute('src', 'https://superafarma.com.br/wp-content/uploads/2020/02/autismo-em-adultos-como.jpg')
+                img.setAttribute('src', 'foto-6.png')
                 img.style.display = "block"
             }else {
                 // idoso
-                img.setAttribute('src', 'https://i0.wp.com/www.kdcare.com.br/wp-content/uploads/2019/01/Acessibilidade-para-idosos-5-dicas-para-deixar-sua-casa-mais-segura.jpg?fit=2500%2C1667&ssl=1')
+                img.setAttribute('src', 'foto-3.png')
                 img.style.display = "block"
             }
             
         }else if (fsex[1].checked){
             genero = 'mulher'
-            if (idade >= 0 && idade < 11){
+            if (idade >= 0 && idade <= 11){
                 // criança
-                img.setAttribute('src', 'p2.png')
+                img.setAttribute('src', 'foto-7.png')
+                img.style.display = "block"
     
-                }else if (idade < 21){
+                }else if (idade >= 12 && idade <= 21){
                     // adolescente
-                
-                }else if (idade < 50 ){
+                    img.setAttribute('src', 'foto-2.png')
+                    img.style.display = "block"
+                }else if (idade >= 22 && idade <= 59 ){
                     // adulto
-    
+                    img.setAttribute('src', 'foto-5.png')
+                    img.style.display = "block"
                 }else {
                     // idoso
+                    img.setAttribute('src', 'foto-1.png')
+                    img.style.display = "block"
                 }
         }
         text.style.textAlign = 'center'
-        text.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        text.innerHTML = `Você é ${genero}, com ${idade} anos.`
     }
 }
