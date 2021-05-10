218ad07c85dfbaf3db08947cec284fa027b7ef8c
Bruno <luna.brsilva@gmail.com>
2021-05-10 11:34:57 -0300

    modificacao: nome dos arquivos

---
+++ while_e_d0_while.js
@@ -0,0 +1,31 @@
+
+var c = 1
+do {
+    console.log(`Oi,como vai? Esse é o Olá de numero ${c}`)
+    c++
+} while (c <= 6)
+
+
+/* 
+
+var c = 1
+while (c <= 6){
+    console.log(`Oi,como vai? Esse é o Olá de numero ${c}`)
+    c++
+}
+Esse exemplo acima é a estrutura de repetição com teste logico no inicio,
+Ou seja: ele faz o teste e forma o bloco, e vai se repetir enquanto for verdadeiro
+No caso acima ele primeiro faz o teste pra depois executar o bloco.
+
+*/
+
+
+// console.log('olá')
+// console.log('olá')
+// console.log('olá')
+// console.log('olá')
+// console.log('olá')
+// console.log('olá')
+// console.log('olá')
+// console.log('olá')
+
