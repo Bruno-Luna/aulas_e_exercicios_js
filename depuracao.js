14f9888ecad7370f3fd30959f648749927756d18
Bruno <luna.brsilva@gmail.com>
2021-05-11 19:17:18 -0300

    add_tabuada

---
+++ depuracao.js
@@ -0,0 +1,8 @@
+//  Depuração ( clica em RUN > Start Debbugging)
+console.log('vai começar...');
+
+for(var b = 4; b <= 7; b++){
+    console.log(b);
+}
+
+console.log('FIM!');
---
+++ for.js
@@ -0,0 +1,18 @@
+//  Repetições com: WHILE, DO WHILE e FOR
+
+var c = 1
+while (c<=3) {
+    console.log(c);
+    c++ 
+} 
+
+var e = 7
+do {
+    console.log(e);
+    e++
+} while (e<=9)
+
+
+for(var b = 15; b <= 20; b++){
+    console.log(b);
+}
