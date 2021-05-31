23da46b8cfbaef2a12966238544e7c8b98cce9fe
Bruno <luna.brsilva@gmail.com>
2021-05-31 15:19:47 -0300

    modificando 'funçao-V'

--- funcao_V.js
+++ funcao_V.js
@@ -1,8 +1,15 @@
 // calculando o fatorial de 5
 
 //  5! = 5 x 4 x 3 x 2 x 1
+//  OU
+//  5! = 5 x 4! (forma recursividade abaixo tambem)
 
 function fatorial(n){
 
-
+        if(n == 1){
+            return 1
+        } else {
+            return n * fatorial(n-1)
+        }
 }
+console.log(fatorial(5));
