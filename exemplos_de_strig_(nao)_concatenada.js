fd7deb4ab3ef4e9095d228250b217eba85550208
Bruno <luna.brsilva@gmail.com>
2021-06-07 18:43:15 -0300

    string_+exemplos_concatenacao

---
+++ exemplos_de_strig_(nao)_concatenada.js
@@ -0,0 +1,13 @@
+
+var ola = "30"  + 2
+console.log(ola);
+// aqui há a concatenação pois a string numeral mais o operador '+' faz com que haja concatenação.  
+
+var oi = "valor" + 2
+console.log(oi);
+
+// aqui há a concatenação pois a string  mais o operador '+' faz com que haja concatenação.  
+
+var oie = "20" * 2
+console.log(oie);
+// aqui não  há a concatenação pois a string numeral  mais o operador '*' faz com que haja a resolução da operação, da mesma forma se dá para os operadores '-' '/' '%'.
